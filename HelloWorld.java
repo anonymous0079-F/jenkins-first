8604b949e95abb6d897e4d88bb327f50b735b77e
supritdas <supritdas789@gmail.com>
2026-08-03 11:42:30 +0530

    Updated Java file to test build automation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins!");
+        System.out.println("Jenkins will automate a new build in two minutes after every commit to the repository.");
+        
     }
 }
